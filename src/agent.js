6c97b59d8d37583ae4b757f81e7625712369023a
acsb-protection <66215450+acsb-protection@users.noreply.github.com>
2021-03-09 07:51:06 -0300

    Update agent.js

--- src/agent.js
+++ src/agent.js
@@ -3,7 +3,7 @@ import _superagent from 'superagent';
 
 const superagent = superagentPromise(_superagent, global.Promise);
 
-const API_ROOT = 'https://backend.cloudcamp.cf:8000/api';
+const API_ROOT = 'https://backend.actech.cf/api';
 
 const encode = encodeURIComponent;
 const responseBody = res => res.body;
